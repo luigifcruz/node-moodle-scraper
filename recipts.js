e7a8ec7e5e76d87151ef17f541c138e769b00b79
Luigi Freitas Cruz <luigifcruz@icloud.com>
2015-12-09 16:32:20 -0200

    Add courses recipe.

--- recipts.js
+++ recipts.js
@@ -42,3 +42,59 @@ function participants(user, course, callback) {
     });
   });
 }
+
+
+// Get Courses list (Just work with simple list of files)
+
+function posts(user, classId, callback) {
+  request.post({url: user.url + "/moodle/login/index.php", form: {username: user.username, password: user.password}}, function(err,httpResponse,body){
+    if (err) throw err
+
+    var AllPosts = [];
+
+    request(user.url + '/moodle/course/view.php?id=' + classId + '&lang=en', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(body);
+
+        $('div.course-content')['0'].children[1].children.forEach(function(e){
+          e.children.forEach(function(e){
+            if (e.attribs.class == "content") {
+              var section = {
+                title: null,
+                summary: null,
+                posts: []
+              }
+              if (e.children[0].children[0] != undefined) {
+                section.title = e.children[0].children[0].data;
+              }
+
+              e.children.forEach(function(e){
+                if (e.name == "ul") {
+                  e.children.forEach(function(e){
+                    // Finally get each course.
+                    var post = {
+                      URL: e.children[0].children[0].children[0].attribs.href,
+                      Name: e.children[0].children[0].children[0].children[1].children[0].data
+                    }
+                     section.posts.push(post)
+                  });
+                }
+              });
+
+              AllPosts.push(section);
+            }
+          });
+        });
+        process.nextTick(function(){
+           callback(null, AllPosts);
+        });
+
+      } else {
+        process.nextTick(function(){
+           callback(new Error(error), null);
+        });
+        return;
+      }
+    });
+  });
+}
